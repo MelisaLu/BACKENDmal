5d564f2fd6a9562c877f37d5b1cc7f6a7309df4d
Melisa Hermann <menima2010@gmail.com>
2022-05-11 21:29:37 -0300

    Se pasa a Angular la maqueta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { AcercademiComponent } from './componentes/acercademi/acercademi.component';
+import { ExplaboralComponent } from './componentes/explaboral/explaboral.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyComponent } from './componentes/proy/proy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    LogoAPComponent,
+    AcercademiComponent,
+    ExplaboralComponent,
+    EstudiosComponent,
+    HabilidadesComponent,
+    ProyComponent
   ],
   imports: [
     BrowserModule,
